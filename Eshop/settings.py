0e16656359e26691f884c22659980738797a2f67
jyoti2390 <jyoti.nagdev12@gmail.com>
2022-07-10 19:23:57 +0100

    static file caching

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIR = [
     os.path.join(BASE_DIR,'static')
