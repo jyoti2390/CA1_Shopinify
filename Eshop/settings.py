99dd4e59e71af455080f2a62d87f6000acc82dbc
jyoti2390 <jyoti.nagdev12@gmail.com>
2022-07-19 11:28:45 +0100

    Website name Changes

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -25,7 +25,7 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['shopinify.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['shopinifi.herokuapp.com','127.0.0.1']
 
 
 # Application definition
